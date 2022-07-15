d60201eb7e7c6328edafe596ff3c2fa2a088fd79
L0cul1t3 <105759501+L0cul1t3HUB@users.noreply.github.com>
2022-07-15 13:27:21 +0900

    Update yumeyuki_fix.user.js

--- yumeyuki_fix.user.js
+++ yumeyuki_fix.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         ユメユキの記号化けを直せるかもしれない
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.0
 // @description  少なくともハートは化けなくなると思います。
 // @author       ロークライト
 // @match        http://futahana.rash.jp/yumeyuki/
+// @downloadURL    https://github.com/L0cul1t3HUB/teiki_userscripts/raw/main/yumeyuki_fix.user.js
+// @updateURL    https://github.com/L0cul1t3HUB/teiki_userscripts/raw/main/yumeyuki_fix.user.js
 // @grant        GM_addStyle
 // ==/UserScript==
 

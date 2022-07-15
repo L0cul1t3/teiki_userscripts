62aa40b8ca98c85a5dd7e08ca5c2560800e7c39e
L0cul1t3 <105759501+L0cul1t3HUB@users.noreply.github.com>
2022-07-15 13:24:08 +0900

    Add files via upload

---
+++ yumeyuki_fix.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         ユメユキの記号化けを直せるかもしれない
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  少なくともハートは化けなくなると思います。
+// @author       ロークライト
+// @match        http://futahana.rash.jp/yumeyuki/
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    GM_addStyle('body { font-family: Trebuchet MS,Verdana,Arial,sans-serif !important; }');
+    // Your code here...
+})();

2c27ce7c3c7d0cf0280b49600b25e8aa8a15f147
L0cul1t3 <105759501+L0cul1t3@users.noreply.github.com>
2024-08-18 11:45:43 +0900

    Update StbRubySupport.user.js

--- StbRubySupport.user.js
+++ StbRubySupport.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         StbRubySupport
-// @version      2024-08-18
+// @version      v0.2
 // @description  記録編集・イベントスペース編集にルビタグ支援のボタンを追加するやつ
 // @author       L0cul1t3
 // @match        https://soraniwa.428.st/stb/*

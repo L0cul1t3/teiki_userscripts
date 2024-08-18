8db3f85464eacd565bd80579b10216627a2927cb
L0cul1t3 <105759501+L0cul1t3@users.noreply.github.com>
2024-08-18 11:37:19 +0900

    Update StbRubySupport.user.js

--- StbRubySupport.user.js
+++ StbRubySupport.user.js
@@ -3,26 +3,28 @@
 // @version      2024-08-15
 // @description  記録編集・イベントスペース編集にルビタグ支援のボタンを追加するやつ
 // @author       L0cul1t3
-// @match        https://soraniwa.428.st/stb/?mode=storyedit*
-// @match        https://soraniwa.428.st/stb/?mode=roomedit*
+// @match        https://soraniwa.428.st/stb/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=428.st
 // @grant        none
 // ==/UserScript==
 /* global jQuery:false */
 
-(function() {
+(($)=> {
   'use strict';
-  const Rb = document.createElement('span');
-  const TargetTxt = document.getElementById('text');
-  Rb.id = "deco_rb";
-  Rb.title="ルビ";
-  Rb.innerHTML = '<img src="https://soraniwa.428.st/stb/img/ruby.png" style="margin:5px;padding:0;">';
-  document.querySelector('span.decoarea span#deco_u').after(Rb);
-  Rb.addEventListener("click",function() {
+  window.on("load", () => {
+    if($("#deco_rb")){
+      const Rb = document.createElement('span');
+      const TargetTxt = document.getElementById('text');
+      Rb.id = "deco_rb";
+      Rb.innerHTML = '<img src="https://soraniwa.428.st/stb/img/ruby.png" style="margin:5px;padding:0;">';
+      document.querySelector('span.decoarea span#deco_u').after(Rb);
+      Rb.on("click", () => {
       TargetTxt.value = TargetTxt.value.substr(0,TargetTxt.selectionStart)
         +"<rb>"
         +TargetTxt.value.substr(TargetTxt.selectionStart,TargetTxt.selectionEnd-TargetTxt.selectionStart)
         +"</rb><rt></rt>"
         +TargetTxt.value.substr(TargetTxt.selectionEnd);
     });
-})();
+    }
+  });
+})(jQuery);
